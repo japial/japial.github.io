d1fa77b8f8e26e7661036891367c142ce3e84815
Jahangir Alom Pial <jahangiralompial@gmail.com>
2023-05-15 14:29:57 +0600

    Spell the number

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,29 +1,51 @@
 function spellNumber(num) {
     const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const twentyToNinety = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    
-    if (num >= 0 && num < 10) {
-      return ones[num];
-    } else if (num >= 10 && num < 20) {
-      return tens[num - 10];
-    } else if (num >= 20 && num < 100) {
-      const tensDigit = Math.floor(num / 10);
-      const onesDigit = num % 10;
-      const tensString = twentyToNinety[tensDigit - 2];
-      const onesString = ones[onesDigit];
-      return onesString ? `${tensString}-${onesString}` : tensString;
-    } else if (num >= 100 && num < 1000) {
-      const hundredsDigit = Math.floor(num / 100);
-      const tensDigit = Math.floor((num % 100) / 10);
-      const onesDigit = num % 10;
-      const hundredsString = `${ones[hundredsDigit]} hundred`;
-      const tensString = tensDigit > 1 ? `${twentyToNinety[tensDigit - 2]}-` : '';
-      const onesString = onesDigit ? ones[onesDigit] : '';
-      return `${hundredsString} ${tensString}${onesString}`.trim();
-    } else {
-      return 'Number out of range';
-    }
+  const tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const twentyToNinety = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const bigNums = ['hundred', 'thousand', 'lakh', 'crore'];
+  
+  if (num >= 0 && num < 10) {
+    return ones[num];
+  } else if (num >= 10 && num < 20) {
+    return tens[num - 10];
+  } else if (num >= 20 && num < 100) {
+    const tensDigit = Math.floor(num / 10);
+    const onesDigit = num % 10;
+    const tensString = twentyToNinety[tensDigit - 2];
+    const onesString = ones[onesDigit];
+    return onesString ? `${tensString}-${onesString}` : tensString;
+  } else if (num >= 100 && num < 1000) {
+    const hundredsDigit = Math.floor(num / 100);
+    const tensDigit = Math.floor((num % 100) / 10);
+    const onesDigit = num % 10;
+    const hundredsString = `${ones[hundredsDigit]} ${bigNums[0]}`;
+    const tensString = tensDigit > 1 ? ` ${twentyToNinety[tensDigit - 2]}` : '';
+    const onesString = onesDigit ? ` ${ones[onesDigit]}` : '';
+    return `${hundredsString}${tensString}${onesString}`.trim();
+  } else if (num >= 1000 && num < 100000) {
+    const divisor = 1000;
+    const quotient = Math.floor(num / divisor);
+    const remainder = num % divisor;
+    const quotientString = spellNumber(quotient) + ` ${bigNums[1]}`;
+    const remainderString = remainder ? ` ${spellNumber(remainder)}` : '';
+    return `${quotientString}${remainderString}`.trim();
+  } else if (num >= 100000 && num < 10000000) {
+    const divisor = 100000;
+    const quotient = Math.floor(num / divisor);
+    const remainder = num % divisor;
+    const quotientString = spellNumber(quotient) + ` ${bigNums[2]}`;
+    const remainderString = remainder ? ` ${spellNumber(remainder)}` : '';
+    return `${quotientString}${remainderString}`.trim();
+  } else if (num >= 10000000 && num < 100000000) {
+    const divisor = 10000000;
+    const quotient = Math.floor(num / divisor);
+    const remainder = num % divisor;
+    const quotientString = spellNumber(quotient) + ` ${bigNums[3]}`;
+    const remainderString = remainder ? ` ${spellNumber(remainder)}` : '';
+    return `${quotientString}${remainderString}`.trim();
+  } else {
+    return 'Number out of range';
+  }
 }
 
   
